a942cd603e7da1dcc77a4eda6c50282ff7f119d8
Ranjith Sekar <ranjith.sekar@clarivate.com>
2020-02-13 18:51:35 +0530

    update

--- sb-xpath-reader/src/main/webapp/WEB-INF/jsp/home.jsp
+++ sb-xpath-reader/src/main/webapp/WEB-INF/jsp/home.jsp
@@ -4,9 +4,12 @@
 <head>
 <meta charset="ISO-8859-1">
 <title>Home Page</title>
+<script type="text/javascript" src="/js/jquery-2.1.3.min.js"></script>
+<script type="text/javascript" src="/js/app.js"></script>
 </head>
 <body>
   <form action="/loadurl" method="post" name="loadurlform">
+    <div id="myDiv">MyDIV</div>
     <table style="width: 100%">
       <tr>
         <td>Enter URL: <input style="width: 80%" type="text" id="enteredurl" name="enteredurl" /></td>
@@ -18,16 +21,27 @@
   </form>
   <table border="1" style="width: 900px; height: 450px">
     <tr>
-      <td><iframe src="${enteredurl}" style="width: 98%; height: 100%"></iframe></td>
+      <td><iframe id="myframe" src="${enteredurl}" style="width: 98%; height: 100%"></iframe></td>
     </tr>
   </table>
   <table>
     <tr>
-      <td>XPATH</td>
+      <td></td>
     </tr>
     <tr>
-      <td><label>xpath here </label></td>
+      <td>XPATH:
+        <div id="xpathvalue"></div>
+      </td>
+    </tr>
+    <tr>
+      <td></td>
+    </tr>
+    <tr>
+      <td>XPATH IFRAME:
+        <div id="xpathvalueiframe"></div>
+      </td>
     </tr>
   </table>
 </body>
 </html>
+
---
+++ sb-xpath-reader/src/main/webapp/js/app.js
@@ -0,0 +1,45 @@
+$.fn.fullSelector = function () {
+    var path = this.parents().addBack();
+    var quickCss = path.get().map(function (item) {
+        var self = $(item),
+            id = item.id ? '#' + item.id : '',
+            clss = item.classList.length ? item.classList.toString().split(' ').map(function (c) {
+                return '.' + c;
+            }).join('') : '',
+            name = item.nodeName.toLowerCase(),
+            index = self.siblings(name).length ? ':child(' + (self.index() + 1) + ')' : '';
+        
+        if (name === 'html' || name === 'body') {
+            return name;
+        }
+        return name + index + id + clss;
+        
+    }).join('/');
+    return quickCss;
+};
+
+$(document).on('click','*', function() {
+	// alert($(this).fullSelector());
+    $('#xpathvalue').text( $(this).fullSelector() );
+    return false;
+});
+
+var $iframe = $('iframe');
+$iframe.load(function(){       
+    var $iframeBody = $iframe.contents().find('body'); 
+
+    alert($iframeBody.height()); 
+
+    /*** 
+    IE6 does not like it when you try an insert an element 
+    that is not made in the target context. 
+    Make sure we create the element with the context of 
+    the iframe's document. 
+    ***/ 
+    var $newDiv = $('<div/>', $iframeBody.get(0).ownerDocument); 
+    $newDiv.css('height', 2000); 
+
+    $iframeBody.empty().append($newDiv); 
+
+    alert($iframeBody.height()); 
+  }); 
